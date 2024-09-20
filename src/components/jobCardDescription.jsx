2a57b0641299faad9c402b84bd8c8bf504242147
Tunmibabs <tunmise01babs@gmail.com>
2024-09-20 19:34:26 +0100

    description start

--- src/components/jobCardDescription.jsx
+++ src/components/jobCardDescription.jsx
@@ -19,8 +19,86 @@ function JobCardDescription() {
 
       <div>
         <h5>Responsibilities for Product Designer</h5>
+        <ul>
+          <li>
+            Create design concepts and drawings to determine the best product
+          </li>
+          <li>
+            Present product ideas to relevant team members for brainstorming
+          </li>
+          <li>
+            Suggest improvements to design and performance to product engineers
+          </li>
+          <li>Employ design concepts into functional prototypes</li>
+          <li>
+            Perform research on product technologies and structures to implement
+            into design concepts
+          </li>
+          <li>
+            Maintain up to date on current industry trends and market conditions
+          </li>
+          <li>
+            Coordinate with designers to ensure accurate communication and
+            efficiency in the design phase
+          </li>
+          <li>
+            Modify and revise existing designs to meet customer expectations
+          </li>
+        </ul>
       </div>
-   </div>
+
+      <div>
+        <h5>Qualifications for Product Designer</h5>
+        <ul>
+          <li>Relevant work experience as a product designer is preferred</li>
+          <li>Exceptional verbal and written communication skills</li>
+          <li>Excellent eye for aesthetic design and customer appeal</li>
+          <li>Good understanding of color science and combinations</li>
+          <li>Artistic and innovative flair</li>
+          <li>Excellent attention to detail</li>
+          <li>Meticulous and diligent</li>
+          <li>Strong knowledge of the industry and market trends</li>
+          <li>Able to sit in front of a computer for long hours at a time</li>
+        </ul>
+      </div>
+
+      <div>
+        <h5>Must Haves</h5>
+        <ul>
+          <li>Relevant work experience as a product designer is preferred</li>
+          <li>Exceptional verbal and written communication skills</li>
+          <li>Excellent eye for aesthetic design and customer appeal</li>
+          <li>Good understanding of color science and combinations</li>
+          <li>Artistic and innovative flair</li>
+          <li>Excellent attention to detail</li>
+          <li>Meticulous and diligent</li>
+          <li>Strong knowledge of the industry and market trends</li>
+          <li>Able to sit in front of a computer for long hours at a time</li>
+        </ul>
+      </div>
+
+      <div>
+        <h5>Benefits of working with us</h5>
+        <ul>
+          <li>Relevant work experience as a product designer is preferred</li>
+          <li>Exceptional verbal and written communication skills</li>
+          <li>Excellent eye for aesthetic design and customer appeal</li>
+          <li>Good understanding of color science and combinations</li>
+          <li>Artistic and innovative flair</li>
+          <li>Excellent attention to detail</li>
+          <li>Meticulous and diligent</li>
+          <li>Strong knowledge of the industry and market trends</li>
+          <li>Able to sit in front of a computer for long hours at a time</li>
+        </ul>
+      </div>
+
+      <h5>Required Education Level: Graduate</h5>
+      <h5>Experience Level: Professional</h5>
+      <h5>Job Type: Remote</h5>
+      <h5>Location: Lagos, NGA</h5>
+      <h5>Salary: </h5>
+
+    </div>
   );
 }
 
